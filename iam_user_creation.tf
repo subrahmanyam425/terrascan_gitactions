bbd289772bf85e9941e96fe194b2b18c83ea393b
subrahmanyam425 <81842364+subrahmanyam425@users.noreply.github.com>
2024-02-14 09:48:51 +0530

    Create iam_user_creation.tf

---
+++ iam_user_creation.tf
@@ -0,0 +1,26 @@
+# Create IAM User
+resource "aws_iam_user" "iam_user" {
+  name = var.aws_iam_user
+
+}
+
+# Create Access Key and Secret Key for IAM User
+resource "aws_iam_access_key" "iam_user_key_pairs" {
+  user = aws_iam_user.iam_user.name
+}
+
+# Store Access Key and Secret Key in AWS SSM
+resource "aws_ssm_parameter" "iam_user_aws_access_key" {
+  name        = "iam-user-accesskey"
+  description = "Access key for IAM User"
+  type        = "SecureString"
+  value       = aws_iam_access_key.iam_user_key_pairs.id
+
+}
+
+resource "aws_ssm_parameter" "iam_user_aws_secret_key" {
+  name        = "iam-user-secretkey"
+  description = "Secret Key for IAM User"
+  type        = "SecureString"
+  value       = aws_iam_access_key.iam_user_key_pairs.secret
+}
