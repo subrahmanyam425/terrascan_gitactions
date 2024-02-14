29a87e6c44aa89eba814ec1653894748aa10c501
subrahmanyam425 <81842364+subrahmanyam425@users.noreply.github.com>
2024-02-14 12:56:55 +0530

    Update iam_user_creation.tf

--- iam_user_creation.tf
+++ iam_user_creation.tf
@@ -5,9 +5,9 @@ resource "aws_iam_user" "iam_user" {
 }
 
 # Create Access Key and Secret Key for IAM User
-#resource "aws_iam_access_key" "iam_user_key_pairs" {
- # user = aws_iam_user.iam_user.name
-#}
+resource "aws_iam_access_key" "iam_user_key_pairs" {
+  user = aws_iam_user.iam_user.name
+}
 
 # Store Access Key and Secret Key in AWS SSM
 resource "aws_ssm_parameter" "iam_user_aws_access_key" {
@@ -23,4 +23,39 @@ resource "aws_ssm_parameter" "iam_user_aws_secret_key" {
   description = "Secret Key for IAM User"
   type        = "SecureString"
   value       = aws_iam_access_key.iam_user_key_pairs.secret
+}
+
+###ec2 related creation
+terraform {
+  backend "s3" {
+    bucket         = "subbu-s3-demo-xyz" # change this
+    key            = "subbu/terraform.tfstate"
+    region         = "ap-south-1"
+    encrypt        = true
+  }
+}
+resource "aws_instance" "ec2" {
+  instance_type = "t2.micro"
+  ami = "ami-03f4878755434977f" 
+  subnet_id = aws_subnet.my_subnet.id
+  tags = {
+    Name = "Session_2_Demo"
+  }
+}
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "Session2_vpc"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  #availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "Session2_subnet"
+  }
 }
